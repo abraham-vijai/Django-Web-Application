4eaf94dc3600c76eadce0ee8afc0a9826a55eb0b
AbrahamVijai <abrahamvijai03@gmail.com>
2024-04-05 15:14:11 -0400

    Created signals.py to create a profile automatically

--- profiles/models.py
+++ profiles/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
     bio = models.TextField(blank = True)
-    avatar = models.ImageField(default =' avatar.png', upload_to='avatars')
+    avatar = models.ImageField(default ='avatar.png', upload_to='avatars')
     updated = models.DateTimeField(auto_now = True)
     created = models.DateTimeField(auto_now_add = True)
 
