b09b434b90f2b37ce2f1366902538be0f43edc39
AbrahamVijai <abrahamvijai03@gmail.com>
2024-04-05 17:30:08 -0400

    Added elements to postBox

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,6 +1,7 @@
 console.log('hello world')
 
 const helloWorldBox = document.getElementById('hello-world')
+const postsBox = document.getElementById('posts-box')
 
 $.ajax({
     type: 'GET',
@@ -18,7 +19,14 @@ $.ajax({
     type: 'GET',
     url: '/data/',
     success: function(response){
-        console.log('success', response)
+        console.log(response)
+        const data = response.data
+        console.log(data)
+        data.forEach(el => {
+            postsBox.innerHTML += `
+                ${el.title} - <b>${el.body}</b><br>
+            `      
+        });
     },
     error: function(error){
         console.log('error', error)
