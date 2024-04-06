56fc3abdca746e525e8c7533140cde012ffe9fb4
AbrahamVijai <abrahamvijai03@gmail.com>
2024-04-06 03:52:01 -0400

    Created post_detail function

--- posts/views.py
+++ posts/views.py
@@ -30,6 +30,17 @@ def post_list_and_create(request):
 
     return render(request, 'posts/main.html', context)
 
+def post_detail(request, pk):
+    obj = Post.objects.get(pk=pk)
+    form = PostForm()
+
+    context = {
+        'obj': obj,
+        'form': form,
+    }
+
+    return render(request, 'posts/detail.html', context)
+
 def load_post_data_view(request, num_posts):
     # if request.is_ajax():
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
@@ -55,7 +66,6 @@ def load_post_data_view(request, num_posts):
 def like_unlike_post(request):
     # if request.is_ajax():
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
-    # if request.method == 'POST':
         pk = request.POST.get('pk')
         obj = Post.objects.get(pk=pk)
         if request.user in obj.liked.all():
