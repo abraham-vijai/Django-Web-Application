dc12e000ca159430ded44b7bfb21355b0c64bada
AbrahamVijai <abrahamvijai03@gmail.com>
2024-04-06 20:07:42 -0400

    Fixed the like unlike functionality

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -77,24 +77,24 @@ const getData = () => {
                 console.log(data)
                 data.forEach(el => {
                     postsBox.innerHTML += `
-                        <div class="card mb-2">
-                            <div class="card-body">
-                                <h5 class="card-title">${el.title}</h5>
-                                <p class="card-text">${el.body}</p>
-                            </div>
-                            <div class="card-footer">
-                                <div class="row">
-                                    <div class="col-2">
-                                        <a href="${url}${el.id}" class="btn btn-primary">Details</a>
-                                    </div>    
-                                    <div class="col-2">
-                                        <form class="like-unlike-forms" data-form-id="${el.id}">
-                                            <a href="#" class="btn btn-primary" id="like-unlike-${el.id}">${el.liked ? `Unlike (${el.count})`: `Like (${el.count})`}</a>
-                                        </form>
-                                    </div>  
+                    <div class="card mb-2">   
+                        <div class="card-body">
+                            <h5 class="card-title">${el.title}</h5>
+                            <p class="card-text">${el.body}</p>
+                        </div>
+                        <div class="card-footer">
+                            <div class ="row">
+                                <div class="col-2">
+                                    <a href="${url}${el.id}" class="btn btn-primary">Details</a>
+                                </div>
+                                <div class="col-2">
+                                    <form class="like-unlike-forms" data-form-id="${el.id}">
+                                        <button class="btn btn-primary" id="like-unlike-${el.id}">${el.liked ? `Unlike (${el.count})`: `Like (${el.count})`}</button>
+                                    </form>
                                 </div>
                             </div>
                         </div>
+                    </div>
                     `      
                 });
                 likeUnlikePosts()
